932311c83dee075695a8fd426db1a54cd7a89db7
Egor Zorin <zorinj@gmail.com>
2021-11-30 02:38:21 +0300

    Add: Layout default

---
+++ src/presentation/layouts/LayoutDefault.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      :mini-variant.sync="mini"
+      permanent
+      expand-on-hover
+    >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-avatar color="primary" size="56"></v-avatar>
+        </v-list-item-avatar>
+
+        <v-list-item-title>Сергей Петров</v-list-item-title>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <slot />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Дашборд", icon: "mdi-home" },
+        { title: "Дома", icon: "mdi-map-outline" },
+        { title: "Добавить", icon: "mdi-plus-circle" },
+        { title: "Календарь", icon: "mdi-calendar" },
+      ],
+      mini: true,
+    };
+  },
+});
+</script>
---
+++ src/presentation/layouts/LayoutLogin.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Login layout</div>
+</template>
