152281b1d9fc688a2f7b34aecf83bceb14f9c0b8
Egor Zorin <zorinj@gmail.com>
2021-12-02 02:40:05 +0300

    Add: dashboard page

--- src/presentation/views/Dashboard.vue
+++ src/presentation/views/Dashboard.vue
@@ -1,11 +1,40 @@
 <template>
-  <div>home</div>
+  <v-layout flex>
+    <v-card
+      v-for="item in menu"
+      :key="item.title"
+      elevation="2"
+      width="253"
+      height="176"
+      class="d-flex flex-column mr-5"
+    >
+      <v-card-title>
+        {{ item.title }} <v-icon class="ml-3">{{ item.icon }}</v-icon>
+      </v-card-title>
+      <v-card-subtitle class="mt-1">{{ item.count }}</v-card-subtitle>
+      <v-spacer></v-spacer>
+      <v-card-actions>
+        <v-btn color="primary" plain>Перейти</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-layout>
 </template>
 
 <script lang="ts">
+import { mdiMapOutline, mdiBank, mdiAccount } from "@mdi/js";
 import Vue from "vue";
 
 export default Vue.extend({
-  name: "Home",
+  data() {
+    return {
+      mdiMapOutline,
+      mdiBank,
+      menu: [
+        { title: "Дома", icon: mdiMapOutline, count: 120 },
+        { title: "Квартиры", icon: mdiBank, count: 570 },
+        { title: "Пользователи", icon: mdiAccount, count: 1000 },
+      ],
+    };
+  },
 });
 </script>
