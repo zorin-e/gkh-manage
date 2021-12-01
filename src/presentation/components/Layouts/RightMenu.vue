88bf7d61a65cfec1832bf1b5c5c9ab33b13105e6
Egor Zorin <zorinj@gmail.com>
2021-12-02 02:07:54 +0300

    Add: right menu

--- src/presentation/components/Layouts/RightMenu.vue
+++ src/presentation/components/Layouts/RightMenu.vue
@@ -1,24 +1,52 @@
 <template>
-  <v-toolbar-items>
+  <div>
+    <v-spacer />
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn :class="$style.noUpperCase" text v-bind="attrs" v-on="on">
+          <v-icon class="mr-2">{{ mdiCog }}</v-icon>
+          Настройки
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item v-for="(item, index) in settingsMenu" :key="index">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
     <v-btn
       :class="$style.noUpperCase"
       text
       :to="{ name: this.routes.signIn.name }"
     >
-      <v-icon left dark>mdi-settings</v-icon>
-      <v-icon aria-hidden="false"> mdi-account </v-icon>
+      <v-icon class="mr-2">{{ mdiPower }}</v-icon>
       Выход
     </v-btn>
-  </v-toolbar-items>
+  </div>
 </template>
 
 <script lang="ts">
 import { ROUTES } from "@/domain/routes";
 import Vue from "vue";
+import { mdiCog, mdiPower } from "@mdi/js";
+
 export default Vue.extend({
   data() {
     return {
       routes: { ...ROUTES },
+      mdiCog,
+      mdiPower,
+      settingsMenu: [
+        {
+          title: ROUTES.companies.title,
+        },
+        {
+          title: ROUTES.employers.title,
+        },
+        {
+          title: ROUTES.services.title,
+        },
+      ],
     };
   },
 });
--- src/presentation/layouts/LayoutDefault.vue
+++ src/presentation/layouts/LayoutDefault.vue
@@ -30,6 +30,17 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app max-height="56" elevation="3" color="white">
+      <v-spacer></v-spacer>
+      <v-col class="d-flex" cols="5">
+        <v-icon class="mr-2">{{ mdiCity }}</v-icon>
+        <v-select
+          :items="companies"
+          label="Управляющая компания"
+          class="mr-10"
+          hide-details
+          :value="1"
+        />
+      </v-col>
       <right-menu />
     </v-app-bar>
     <v-main class="pl-14 pt-14">
@@ -43,7 +54,13 @@
 <script lang="ts">
 import Vue from "vue";
 import RightMenu from "@/presentation/components/Layouts/RightMenu.vue";
-import { mdiMapOutline, mdiHome, mdiPlusCircle, mdiCalendar } from "@mdi/js";
+import {
+  mdiMapOutline,
+  mdiHome,
+  mdiPlusCircle,
+  mdiCalendar,
+  mdiCity,
+} from "@mdi/js";
 export default Vue.extend({
   components: {
     RightMenu,
@@ -57,7 +74,9 @@ export default Vue.extend({
         { title: "Заявки", icon: mdiPlusCircle },
         { title: "Календарь", icon: mdiCalendar },
       ],
+      companies: [{ text: "Интехсервис", value: 1 }],
       mini: true,
+      mdiCity,
     };
   },
 });
