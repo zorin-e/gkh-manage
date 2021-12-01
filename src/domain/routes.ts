7d3fbe87395bb158b8c958d3d7b417fcfde5670b
Egor Zorin <zorinj@gmail.com>
2021-12-02 01:37:49 +0300

    Add: routes

--- src/domain/routes.ts
+++ src/domain/routes.ts
@@ -1,4 +1,8 @@
-export const ROUTES = {
+type Routes = {
+  [key: string]: { title: string; name: string };
+};
+
+export const ROUTES: Routes = {
   signIn: {
     title: "Вход",
     name: "SignIn",
@@ -18,5 +22,17 @@ export const ROUTES = {
   calendar: {
     title: "Календарь",
     name: "Calendar",
+  },
+  companies: {
+    title: "Управляющие компании",
+    name: "Companies",
+  },
+  employers: {
+    title: "Сотрудники",
+    name: "Employers",
+  },
+  services: {
+    title: "Услуги",
+    name: "Services",
   },
 };
