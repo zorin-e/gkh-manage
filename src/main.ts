2747db7e08bb890f5e2e87fa3ea27cf0ad3b4e20
Egor Zorin <zorinj@gmail.com>
2021-12-09 23:25:25 +0300

    Add: init notification

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "@/styles/common.scss";
+import Notifications from "vue-notification";
 
+Vue.use(Notifications);
 Vue.config.productionTip = false;
 
 new Vue({
