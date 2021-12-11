cf323dd5fc8c78a4c0ffb983010f4aa48473120b
Egor Zorin <zorinj@gmail.com>
2021-12-12 00:14:22 +0300

    Add: refresh token before app mounted

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,17 @@ import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "@/styles/common.scss";
 import Notifications from "vue-notification";
+import { refreshTokenFn } from "./bootstrap";
 
 Vue.use(Notifications);
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
-}).$mount("#app");
+(async () => {
+  await refreshTokenFn();
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+  }).$mount("#app");
+})();
